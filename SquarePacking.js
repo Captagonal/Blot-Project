04b9cd9237c90e37e3ba83d1c238cc6b09d21e00
Captagonal <KOC540916@stu.aasd.k12.wi.us>
2024-08-04 14:13:12 -0500

    Finished 17 square Packing

--- SquarePacking.js
+++ SquarePacking.js
@@ -30,5 +30,29 @@ drawLines(square);
 square = bt.translate(square, [0,-125+(2*s)]);
 drawLines(square);
 square = bt.rotate(square, 50.2582715)
-square = bt.translate(square, [-73.399649,21.168876]);
+square = bt.translate(square, [-73.399649,21.168877]);
+drawLines(square);
+square = bt.translate(square, [22.826509,15.644229]);
+drawLines(square);
+square = bt.translate(square, [-42.285322,2.500477]);
+drawLines(square);
+square = bt.translate(square, [61.952169,15.8199]);
+drawLines(square);
+square = bt.translate(square, [-39.193448 ,0.068831]);
+drawLines(square);
+square = bt.translate(square, [19.699835 ,18.253954]);
+drawLines(square);
+//reset position
+square = [
+    [[0, 0], [0, s]],
+    [[0, s], [s, s]],
+    [[s, s], [s, 0]],
+    [[s, 0], [0, 0]]
+];
+square = bt.translate(square, [49.214949,0]);
+drawLines(square);
+square = bt.translate(square, [48.992003,41.721721]);
+drawLines(square);
+square = bt.rotate(square, -53.3080943)
+square = bt.translate(square, [-24.973365,-20.244737]);
 drawLines(square);
