7fc8f43df020d9b3138bc370507df124a1d580c3
Captagonal <KOC540916@stu.aasd.k12.wi.us>
2024-08-04 09:37:38 -0500

    1 more square

--- SquarePacking.js
+++ SquarePacking.js
@@ -7,7 +7,7 @@ const width = 125;
 const height = 125;
 setDocDimensions(width, height);
 
-const s = 125 / 4.67553009360455 
+const s = 125 / 4.67553009360455 //26.7349364666
 var square = [
     [[0, 0], [0, s]],
     [[0, s], [s, s]],
@@ -28,4 +28,7 @@ drawLines(square);
 square = bt.translate(square, [0,-s]);
 drawLines(square);
 square = bt.translate(square, [0,-125+(2*s)]);
+drawLines(square);
+square = bt.rotate(square, 50.2582715)
+square = bt.translate(square, [-73.399649,21.168876]);
 drawLines(square);
